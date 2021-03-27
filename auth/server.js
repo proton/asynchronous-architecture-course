120f70bc2a77b74732f8341e4d30d203fb00ad28
Petr Savichev (proton) <psavichev@gmail.com>
2021-03-27 17:23:06 -0500

    implemented /users.json

--- auth/server.js
+++ auth/server.js
@@ -4,16 +4,32 @@ const app = express()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost:27017/popug_auth', { useNewUrlParser: true, useUnifiedTopology: true })
 
+const userSchema = new mongoose.Schema({
+  login: String,
+  password: String,
+  role: String
+})
+
+const User = mongoose.model('User', userSchema)
+
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
   console.log('connected to db')
 })
 
+// const admin = new User({ login: 'admin', password: 'password', role: 'admin' })
+// admin.save((err, _user) => {
+//   if (err) return console.error(err)
+// })
+
 app.listen(3001, _ => {
   console.log('listening on 3001')
 })
 
-app.get('/', (_req, res) => {
-  res.send('Hello World')
+app.get('/users.json', (_req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return console.error(err)
+    res.send(users)
+  })
 })
