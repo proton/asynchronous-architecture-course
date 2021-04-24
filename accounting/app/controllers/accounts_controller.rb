4c0d13123b309861fde583accdf712208f971bc8
Petr Savichev (proton) <psavichev@gmail.com>
2021-04-24 09:26:37 -0400

    api for getting own balance

--- accounting/app/controllers/accounts_controller.rb
+++ accounting/app/controllers/accounts_controller.rb
@@ -4,4 +4,9 @@ class AccountsController < ApplicationController
   def index
     @accounts = Account.all
   end
+
+  def my
+    account = Account.find_by(user_id: current_user.id)
+    render json: { balance: account.balance }
+  end
 end
--- accounting/config/routes.rb
+++ accounting/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'accounts#index'
+
+  get '/my', to: 'accounts#my'
 end
